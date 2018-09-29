b38a8d6238baa47c6f223c32af71796c3cd38e47
Grant <pydata314@gmail.com>
2018-09-29 08:55:10 -0700

    Add files via upload

---
+++ euler_48.py
@@ -0,0 +1,10 @@
+# The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+#
+# Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+
+sum = 0
+
+for i in range(1,1001):
+    sum += i **i
+
+print(sum%10000000000)

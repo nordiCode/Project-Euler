3573f38dcfece8b115c1f56c271e684d61bb7968
Grant <pydata314@gmail.com>
2018-09-29 19:36:48 -0700

    Add files via upload

---
+++ euler_25.py
@@ -0,0 +1,17 @@
+#First fibonacci term with over 1000 digits
+
+term_1  = 1
+term_2 = 1
+previous = 1
+limit = 100000000
+#print(previous)
+#print(10**999)
+count =1
+while previous < 10**999:
+    count +=1
+    previous = term_2 + term_1
+    term_1 = term_2
+    term_2 = previous
+    #print(previous)
+    if previous > 10**999:
+        print(count)
